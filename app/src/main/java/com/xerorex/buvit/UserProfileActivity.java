3feb85ed526fa4a0cff1fa9ba9b3182453cdb44f
GreatStoneDragon <abdullah.alshehabi@gmail.com>
2015-11-24 00:35:10 -0800

    Changed layout orientation for main, different button images, and completed UserProfile functionalities

--- app/src/main/java/com/xerorex/buvit/UserProfileActivity.java
+++ app/src/main/java/com/xerorex/buvit/UserProfileActivity.java
@@ -1,49 +1,38 @@
 package com.xerorex.buvit;
 
-import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ScaleDrawable;
 import android.media.Image;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
+    private RelativeLayout userProfileLayout;
+    private GridLayout gridLayout;
+
+    private int WRAP_CONTENT = RelativeLayout.LayoutParams.WRAP_CONTENT;
     private final int NUMBER_OF_PUNCHES = 9;
     private ImageButton[] arrayOfButtons;
-    ImageButtonListener buttonListener;
-    Drawable punchImageUnpressed;
-    Drawable punchImagePressed;
+    private ImageButtonListener buttonListener;
+    public int updatePunchNumbers = 0;
+
+    int padding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-//        float imageButtonDimension = (Float) getResources().getDimension(R.dimen.user_profile_activity_button_dimen);
-//        Drawable punchUnpressed = getDrawable(R.drawable.user_profile_punch_unpressed);
-//        Drawable punchPressed = getDrawable(R.drawable.user_profile_punch_pressed);
-//
-//        punchImageUnpressed = resizeImageButton(punchUnpressed, imageButtonDimension);
-//        punchImagePressed = resizeImageButton(punchPressed, imageButtonDimension);
-
         setUserProfileTitle();
         addPunchMarks();
+        addSaveButton();
     }
 
     private void setUserProfileTitle() {
@@ -53,10 +42,8 @@ public class UserProfileActivity extends AppCompatActivity {
 
     private void addPunchMarks() {
 
-        int WRAP_CONTENT = RelativeLayout.LayoutParams.WRAP_CONTENT;
-
-        //Relative layout from xml file reference pull up
-        RelativeLayout userProfileLayout = (RelativeLayout) findViewById(R.id.user_profile_acitivity_layout);
+        //Relative layout from xml file reference
+        userProfileLayout = (RelativeLayout) findViewById(R.id.user_profile_acitivity_layout);
         arrayOfButtons = new ImageButton[NUMBER_OF_PUNCHES];
 
         //ButtonLayoutParams params configuration
@@ -66,23 +53,24 @@ public class UserProfileActivity extends AppCompatActivity {
         //Gridlayout params configuration
         RelativeLayout.LayoutParams gridLayoutParams = new RelativeLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
         gridLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        gridLayoutParams.addRule(RelativeLayout.BELOW, R.id.search_user_activity_title);
 
         //GridLayout setup
-        GridLayout gridLayout = new GridLayout(getApplicationContext());
+        gridLayout = new GridLayout(getApplicationContext());
         gridLayout.setColumnCount(5);
         gridLayout.setRowCount(2);
-        gridLayoutParams.addRule(RelativeLayout.BELOW, R.id.search_user_activity_title);
         gridLayout.setLayoutParams(gridLayoutParams);
+        gridLayout.setId(201);
 
         userProfileLayout.addView(gridLayout);
 
         int counter = 100;
-        int padding = Math.round((Float) getResources().getDimension(R.dimen.user_profile_activity_button_padding));
+        padding = Math.round((Float) getResources().getDimension(R.dimen.user_profile_activity_button_padding));
 
         buttonListener = new ImageButtonListener();
 
         //For loop populating gridlayout with buttons
-        for(ImageButton thisButton: arrayOfButtons){
+        for (ImageButton thisButton : arrayOfButtons) {
             thisButton = new ImageButton(this);
 
             thisButton.setPadding(padding, padding, padding, padding);
@@ -104,6 +92,8 @@ public class UserProfileActivity extends AppCompatActivity {
         freePunch.setBackgroundColor(Color.WHITE);
 
         freePunch.setImageResource(R.drawable.user_profile_punch_free_false);
+        freePunch.setTag(R.drawable.user_profile_punch_free_false);
+        freePunch.setOnClickListener(buttonListener);
 
         freePunch.setId(counter++);
         gridLayout.addView(freePunch);
@@ -111,54 +101,74 @@ public class UserProfileActivity extends AppCompatActivity {
 
     }
 
-    private class ImageButtonListener implements View.OnClickListener{
+    private class ImageButtonListener implements View.OnClickListener {
 
-        public void onClick(View view){
+        boolean freePunch = false;
 
-            if(allPreviousPressed(view) && !view.getTag().equals(R.drawable.user_profile_punch_pressed)){
-                ImageButton imageButton = (ImageButton) view;
-                imageButton.setImageResource(R.drawable.user_profile_punch_pressed);
-                imageButton.setTag(R.drawable.user_profile_punch_pressed);
-                imageButton.setOnClickListener(buttonListener);
+        public void onClick(View view) {
+
+            int button = view.getId();
+            ImageButton imageButton = (ImageButton) view;
+
+            switch (button) {
+                case 109:
+                    if (view.getTag().equals(R.drawable.user_profile_punch_free_false) && updatePunchNumbers == 9) {
+                        imageButton.setImageResource(R.drawable.user_profile_punch_free_true);
+                        imageButton.setTag(R.drawable.user_profile_punch_free_true);
+                        imageButton.setOnClickListener(buttonListener);
+                        freePunch = true;
+                    } else if (view.getTag().equals(R.drawable.user_profile_punch_free_true) && updatePunchNumbers == 9) {
+                        imageButton.setImageResource(R.drawable.user_profile_punch_free_false);
+                        imageButton.setTag(R.drawable.user_profile_punch_free_false);
+                        imageButton.setOnClickListener(buttonListener);
+                        freePunch = false;
+                    }
+                    break;
+
+                default:
+                    if (view.getTag().equals(R.drawable.user_profile_punch_unpressed) && freePunch != true) {
+                        imageButton.setImageResource(R.drawable.user_profile_punch_pressed);
+                        imageButton.setTag(R.drawable.user_profile_punch_pressed);
+                        imageButton.setOnClickListener(buttonListener);
+                        updatePunchNumbers++;
+                    } else if (view.getTag().equals(R.drawable.user_profile_punch_pressed) && freePunch != true) {
+                        imageButton.setImageResource(R.drawable.user_profile_punch_unpressed);
+                        imageButton.setTag(R.drawable.user_profile_punch_unpressed);
+                        imageButton.setOnClickListener(buttonListener);
+                        updatePunchNumbers--;
+                        break;
+                    }
+
             }
-            else if (view.getTag().equals(R.drawable.user_profile_punch_pressed)){
-                ImageButton imageButton = (ImageButton) view;
-                imageButton.setImageResource(R.drawable.user_profile_punch_unpressed);
-                imageButton.setTag(R.drawable.user_profile_punch_unpressed);
-                imageButton.setOnClickListener(buttonListener);
-            }
 
-        }
-
-        private boolean allPreviousPressed(View imageButtonView){
-
-            Log.d("testing", Integer.toString(imageButtonView.getId()));
-
-            int counter = 1;
-            for(int i = imageButtonView.getId(); i > 100; i--){
-                Drawable previous = getResources().getDrawable(imageButtonView.getId() - counter++);
-                Drawable compare = getResources().getDrawable(R.drawable.user_profile_punch_unpressed);
-
-                if(compare.equals(previous)) {
-                    Log.d("testing", "False");
-                    return false;
-                }
-            }
-            return true;
         }
 
     }
 
-//    private Drawable resizeImageButton(Drawable image, float scaleFactor) {
-//
-//        int newWidth = Math.round(image.getIntrinsicWidth() * scaleFactor);
-//        int newHeight =  Math.round(image.getIntrinsicHeight() * scaleFactor);
-//
-//        image.setBounds(0, 0, newWidth, newHeight);
-//        ScaleDrawable sd = new ScaleDrawable(image, 0, newWidth, newHeight);
-//        sd.setLevel(8000);
-//
-//        return sd;
-//    }
+    //Adds an account save button which will update the userProfile object
+    private void addSaveButton() {
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        //Uses id that i set because the gridLayout.getId() didnt work for some reason
+        layoutParams.addRule(RelativeLayout.BELOW, 201);
+
+        ImageButton saveButton = new ImageButton(getApplicationContext());
+        saveButton.setImageResource(R.drawable.user_profile_save_button);
+        saveButton.setTag(R.drawable.user_profile_save_button);
+        saveButton.setLayoutParams(layoutParams);
+        saveButton.setBackgroundColor(Color.WHITE);
+        saveButton.setPadding(padding, padding, 30, padding);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("testing", "save button clicked");
+            }
+        });
+
+        userProfileLayout.addView(saveButton);
+
+    }
 
 }
